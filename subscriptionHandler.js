cfe27f7cd61ee42ece7e34ddb668ce52d3e066af
vvquang <vv.quang@1pac.vn>
2020-11-02 09:39:26 +0700

    create project

---
+++ subscriptionHandler.js
@@ -0,0 +1,52 @@
+const subscriptions = {};
+var crypto = require('crypto');
+const webpush = require('web-push');
+
+const vapidKeys = {
+  privateKey: 'bdSiNzUhUP6piAxLH-tW88zfBlWWveIx0dAsDO66aVU',
+  publicKey:
+    'BIN2Jc5Vmkmy-S3AUrcMlpKxJpLeVRAfu9WBqUbJ70SJOCWGCGXKY-Xzyh7HDr6KbRDGYHjqZ06OcS3BjD7uAm8',
+};
+
+webpush.setVapidDetails(
+  'mailto:example@yourdomain.org',
+  vapidKeys.publicKey,
+  vapidKeys.privateKey
+);
+
+function createHash(input) {
+  const md5sum = crypto.createHash('md5');
+  md5sum.update(Buffer.from(input));
+  return md5sum.digest('hex');
+}
+
+function handlePushNotificationSubscription(req, res) {
+  const subscriptionRequest = req.body.data;
+  const susbscriptionId = createHash(JSON.stringify(subscriptionRequest));
+  subscriptions[susbscriptionId] = subscriptionRequest;
+  res.status(201).json({ id: susbscriptionId });
+}
+
+function sendPushNotification(req, res) {
+  const subscriptionId = req.params.id;
+  const pushSubscription = subscriptions[subscriptionId];
+  webpush
+    .sendNotification(
+      pushSubscription,
+      JSON.stringify({
+        title: 'Title NOTIFICATION !!!',
+        text:
+          "This is text of notification...Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+        image: '/images/jason-leung-HM6TMmevbZQ-unsplash.jpg',
+        tag: 'new-product',
+        url: '/new-product-jason-leung-HM6TMmevbZQ-unsplash.html',
+      })
+    )
+    .catch((err) => {
+      console.log(err);
+    });
+
+  res.status(202).json({});
+}
+
+module.exports = { handlePushNotificationSubscription, sendPushNotification };
