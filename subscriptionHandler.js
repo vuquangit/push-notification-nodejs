c473d3514e8d38ae7821663dd5d705e52ed1db13
vuquangit <vuquangit@gmail.com>
2021-03-26 16:13:39 +0700

    Modify content notification send

--- subscriptionHandler.js
+++ subscriptionHandler.js
@@ -37,9 +37,9 @@ function sendPushNotification(req, res) {
         title: 'Punch NOTIFICATION !!!',
         text:
           "This is text of notification...Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
-        image: '/assets/images/notification.png',
+        image: '/logo.png',
         tag: 'new-punch',
-        url: '/calendar',
+        url: '/',
       })
     )
     .catch((err) => {
